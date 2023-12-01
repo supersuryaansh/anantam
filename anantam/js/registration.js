733b18c0a3934c030e27eda6a02ad7d69d187055
supersuryaansh <prod.genx@gmail.com>
2023-12-01 22:32:26 +0530

    Testing .gitignore

---
+++ anantam/js/registration.js
@@ -0,0 +1,3 @@
+const registrationCloseBtn = document.querySelector(".menu--close-btn");
+const registrationPage = document.querySelector(".menuPage");
+const registrationOpenBtn = document.querySelector(".");
