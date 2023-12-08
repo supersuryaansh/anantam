95536ae9be3854cbd204210c809e5c124223fa76
supersuryaansh <prod.genx@gmail.com>
2023-12-08 23:37:38 +0530

    Added dynamic problem statements

--- includes/views/hackathon.php
+++ includes/views/hackathon.php
@@ -1,4 +1,8 @@
 <?php
+if(!defined('LIFE')){
+    die();
+}
+
 if (!isset($_GET['problemId'])){
 ?>
 <style>
@@ -80,222 +84,217 @@ if (!isset($_GET['problemId'])){
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>1</td>
-        <td data-index="130" class="event--id">AN130</td>
-        <td>Software</td>
-        <td>
-          <details>
-            <summary>
-              Development of software application for analysis and processing of
-              dvbs2 receiver output stream i.e., raw BB Frames, GSE and TS in
-              near real time.
-            </summary>
-            <p>
-              Classification & identification of audio, video, data & protocol
-              such as MPE, ULE, SIP, RTP, FTP, SFTP, HTTP, HTTPS, SNMP, POP,
-              SMTP, SSH etc. 2. Classification Identification of
-              encryption/scrambling if present in stream through headers and SI
-              tables 3. Extraction of VoIP calls, audio and video programs,
-              file, email, web-page etc in separate files. 4. Decoding and
-              playing selected audio/video contents Preferred Language- Python,
-              Lab-view, C/C++, VHDL/Verilog Expected Outcome-
-              Algorithm/Software/GUI Preferred Platform- Windows/Linux
-            </p>
-          </details>
-        </td>
-        <!-- <td><button class="AN--btn">APPLY</button></td> -->
-        <td><button>APPLY</button></td>
-      </tr>
 
-      <tr>
-        <td>2</td>
-        <td data-index="212">AN212</td>
-        <td>Software</td>
+    <?php
+    //parse the csv file
+    $csvParser = new CSVParser($server_root.'/uploads/extras/problem_statements.csv', ['ID', 'Title', 'Bucket' ,'Category', 'Description' ]);
+    $parsedData = $csvParser->parseCSV();
 
-        <td>
-          <details>
-            <summary>Change detection due to human activities.</summary>
-            <p>
-              Using satellite imagery, create an automated system for detecting
-              change related only to human activities from satellite imagery.
-              i.e. Develop AI/ML based model for change detection of only
-              man-made objects like vehicles, buildings, roads, aircraft etc.
-              from remote sensing images Data: Sentinel-2, LISS-4
-            </p>
-          </details>
-        </td>
-        <td><button>APPLY</button></td>
-      </tr>
-
-      <tr>
-        <td>3</td>
-        <td data-index="321">AN321</td>
-        <td>Software</td>
-
-        <td>
-          <details>
-            <summary>
-              Smart and Effective realtime Management of street parking
-            </summary>
-            <p>
-              ln lndia, the number of vehicles is constanfly on the rise, while
-              the availability of parking space in cities has not kept pace with
-              the increasing vehicle numbers, primarily due to the rapid groMh
-              in population. Consequenfly, issues such as traffic congestion,
-              narrowed streets due to on-street parking, a mismatch between
-              parking supply and demand, and illegal parking have become all too
-              familiar in lndian cities. Parking space has become a scarce
-              commodity, and instead of simply increasing the number of
-              available parking spaces, it is crucial to employ effective
-              technology-based solutions to optimize their utilization. Smart
-              parking solutions, utilizing sensors and software, can provade
-              realtime information on available parking spaces to both city
-              officials and drivers. Furthermore, leveraging emerging
-              technologies can help optimize parking prices. A low parking price
-              may encourage more vehicles on the road, leading to increased air
-              and noise pollution, whereas too high prices may generate less
-              revenue and less efectuve resource usage.Apart from the loss of
-              revenue for operators, there is economic downside to the business
-              in vicinity, effect on government taxes, employment.At the same
-              parking space area and costs also have a corelation to balance
-              optimum usage and revenue. Therefore, setting the right price for
-              parking based on demand and optimizing occupancy is the best
-              approach. The primary objective of the solution should be to equip
-              city administrators with an effective parking management tool that
-              can predict, manage, and finance parking in cities. An app should
-              be developed to allow citizens to conveniently reserve pce king
-              spots and make payments based on dynamic pricing This not only
-              saves citizens' time spent searching the parking but also reduces
-              environmental degradation resulting from congestion caused by
-              parking and provides a sustainable source of revenue for the city
-              administration. We need an innovative, simple and widely coveraged
-              parking needs in the city and also become most compliant for
-              Traffic and mobility needs.
-            </p>
-          </details>
-        </td>
-        <td><button>APPLY</button></td>
-      </tr>
-      <tr>
-        <td>4</td>
-        <td data-index="135">AN135</td>
-        <td>Software</td>
-        <td>
-          <details>
-            <summary>
-              Development of Explainable AI (XAI) based model for prediction of
-              heavy /high impact rain events using satellite data
-            </summary>
-            <p>
-              Nowcasting of heavy precipitation rainfall events with an
-              understanding of the most important predictors and also an idea as
-              to why a certain model can fail. Desired Outcome- The developed
-              system shall provide the following: 1. AI based model to predict
-              particular rain episodes of greater impact using satellite data
-              (INSAT-3D/3DR) . 2. An explainable module into the AI model (XAI)
-              3. The final output should be in terms of a web application, with
-              associated accuracy of the models worked on and an explainable
-              component of the outputs.
-            </p>
-          </details>
-        </td>
-        <td><button>APPLY</button></td>
-      </tr>
-
-      <tr>
-        <td>5</td>
-        <td data-index="234">ANF234</td>
-        <td>Hardware</td>
-        <td>
-          <details>
-            <summary>
-              Frequent dislodgement of belt conveyor along hilly terrain for
-              various reasons
-            </summary>
-            <p>
-              The 14.6 km long cable belt conveyor passing over complex hilly
-              terrain suffers belt dislodgements due to various reasons
-              resulting in the sudden loss of production. Solution Desired:
-              Capture reasons for belt dislodgements across its 14.6 KM length
-              from past data and using suitable ML software, prediction of belt
-              dislodgements should be done beforehand to take corrective and
-              preventive actions.
-            </p>
-          </details>
-        </td>
-        <td><button>APPLY</button></td>
-
-        <!-- Centralized Monitoring System for Street Light Fault Detection and Location Tracking -->
-      </tr>
-
-      <tr>
-        <td>6</td>
-        <td data-index="594">ANF594</td>
-        <td>Hardware</td>
-        <td>
-          <details>
-            <summary>
-              Centralized Monitoring System for Street Light Fault Detection and
-              Location Tracking
-            </summary>
-            <p>
-              Electricity is the critical need for progress of the livelihood.ln
-              many Indian cities, the maintenance of street lights has become a
-              challenging and inefficient process due to the lack of a
-              centralized monitoring system. ldentifying faults, such as non-
-              functioning lights, current leakage and cable breakage, relies on
-              citizen grievances, leading to delays, increased costs, and safety
-              concerns. Linemen spend valuable time manually searching for
-              faults, diagnosing issues, and fixing them, which can take several
-              days to complete. The absence of precise fault location
-              information further complicates the process. To overcome these
-              obstacles, we seek an innovative solution that provides realtime
-              fault detection, accurate identification of fault types, and
-              precise location tracking of faulty street lights. This solution
-              aims to empower linemen with efficient fault management
-              capabilities, reducing their workload and ensuring timely
-              maintenance. Moreover, it should enable the local authorities to
-              proactively address faults, enhance service quality, and optimize
-              street light maintenance processes in their respective cities..The
-              prime aim of this problem statement is to develop a "Automated
-              Defect Detection and Prevention Assistance with Effective
-              Governance for Cities in India"""
-            </p>
-          </details>
-        </td>
-        <td><button>APPLY</button></td>
-
-        <!-- Centralized Monitoring System for Street Light Fault Detection and Location Tracking -->
-      </tr>
-
-      <tr>
-        <td>7</td>
-        <td data-index="669">ANF669</td>
-        <td>Hardware</td>
-        <td>
-          <details>
-            <summary>
-              Design of RF Up/Down-converter for signals using GNU Radio and
-              SDRs
-            </summary>
-            <p>
-              The 14.6 km long cable belt conveyor passing over complex hilly
-              terrain suffers belt dislodgements due to various reasons
-              resulting in the sudden loss of production. Solution Desired:
-              Capture reasons for belt dislodgements across its 14.6 KM length
-              from past data and using suitable ML software, prediction of belt
-              dislodgements should be done beforehand to take corrective and
-              preventive actions.
-            </p>
-          </details>
-        </td>
-        <td><button>APPLY</button></td>
-
-        <!-- Centralized Monitoring System for Street Light Fault Detection and Location Tracking -->
-      </tr>
-      <!-- Add more rows as needed -->
+        //loop over all the rows
+        $i = 1;
+        foreach ($parsedData as $problemArray){?>
+            <tr>
+                <td><?= $i ?></td>
+                <td data-index="130" class="event--id"><?= $problemArray['ID'] ?></td>
+                <td><?= $problemArray['Bucket'] ?></td>
+                <td>
+                    <details>
+                        <summary>
+                            <?= $problemArray['Title'] ?>
+                        </summary>
+                        <p>
+                            <?= $problemArray['Description'] ?>
+                        </p>
+                    </details>
+                </td>
+                <td><form method="get" action="dashboard?action=hackathon"><input type="hidden" name="action" value="hackathon"><input type="hidden" name="problemId" value="<?= $problemArray['ID'] ?>"> <button type="submit">APPLY</button></form></td>
+            </tr>
+        <?php $i +=1;} //end for each    ?>
     </tbody>
   </table>
 </div>
-<?php } ?>
+<?php } //end if statement with get of ID
+    else{
+##########
+### SECOND FILE LOADS HERE
+#########
+echo var_dump($session->getName());
+//verify input and register
+if(isset($_POST['hackathon-submit'])){
+    if(!empty($_POST['usrTeamName'])  && !empty($_POST['usrProblemSt'])  && !empty($_FILES['usrPresentation']['name']) ){
+        $fileUploader = new FileUploader($server_root."/uploads/presentations/");
+        //random chars for prefix
+        $fileName = "presentation_". substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 20) ."_".basename($_FILES["usrPresentation"]["name"]);
+        $fileUploader->uploadFile($_FILES["usrPresentation"],$fileName,array('pptx', 'ppt', 'docx','doc','pdf'));
+
+        if($fileUploader->uploadOk) {
+            //handle registration
+            $register = new EventRegister();
+            $register->hackathon($_POST['usrTeamName'],$_POST['usrProblemSt'],$_FILES['usrPresentation']['name'],$session->getName());
+
+        }else{
+            echo "Something went wrong";
+        }
+
+    }//end form value check
+}//end submit form check
+
+?>
+
+<style>
+    @font-face {
+        font-family: noken;
+        src: url(../assets/fonts/nokenmedium.otf);
+    }
+    @font-face {
+        font-family: medium;
+        src: url(../assets/fonts/nokenItalic.otf);
+    }
+    @font-face {
+        font-family: semiBold;
+        src: url(../assets/fonts/nockenSemiBold.otf);
+    }
+    @font-face {
+        font-family: clock;
+        src: url(../assets/fonts/CLOCK.ttf);
+    }
+    main {
+        font-family: noken;
+    }
+    .event--section {
+        background-color: #f6f6f6 !important;
+        width: 100%;
+        height: 80vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding-left: 20px !important;
+        margin: 0px !important;
+    }
+    .d--card--area {
+        display: flex !important;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        background-color: transparent;
+    }
+    .event--top-line img, .event--top-line h1{
+        display: none;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: baseline;
+        justify-content: center;
+        /* width: 70%; */
+        padding-inline: 20px;
+        gap: 20px;
+    }
+    input {
+        padding: 14px 0px;
+        border: 0px;
+        border-bottom: 2px solid var(--color);
+        background-color: transparent;
+    }
+    .submit--btn {
+        border: 2px solid var(--color);
+        padding: 14px 30px;
+        transition: 0.4s;
+    }
+    .submit--btn:hover {
+        background-color: #ffca2b;
+    }
+    select {
+        width: 40%;
+        padding: 14px 0px;
+        border: 2px solid var(--color);
+    }
+    select:focus {
+        outline: none;
+    }
+    .hackathon--poster-img{
+        max-width: 500px;
+    }
+</style>
+<div class="event--section">
+  <span class="event--top-line">
+    <img class="line3"
+         id="line3"
+         src="../assets/images/webLINES/EventLine.svg"
+         alt="" />
+    <h1>/// EVENTS</h1>
+  </span>
+    <div class="card--area d--card--area">
+        <img class="hackathon--poster-img" src="/assets/images/POSTERS/ANANT NETRUNN POSTER.svg" alt="">
+        <form action="/dashboard/?action=hackathon" method="post" enctype="multipart/form-data">
+
+            <input type="hidden" name="action" value="hackathon" />
+            <h1>ANANT NETRUNN REGISTRATION :</h1>
+            <!-- Team Name -->
+            <span>
+        <label for="usrTeamName">TEAM NAME : </label>
+        <input type="text" id="usrTeamName" name="usrTeamName" required />
+      </span>
+            <!-- Problem Statements -->
+            <span>
+        <label for="usrProblemStSelect">SELECT PROBLEM STATEMENT : </label>
+        <select name="usrProblemSt" id="usrProblemStSelect" required>
+          <option selected disabled>Select your problem statement</option>
+          <option value="001">
+            Development of software application for analysis and processing of
+            dvbs2 receiver output stream.
+          </option>
+          <option value="003">Quantum Secure Email Client Application</option>
+          <option value="004">Change detection due to human activities.</option>
+          <option value="005">Automatic Drug Dispenser</option>
+          <option value="006">
+            Blended Learning to overcome inadequate infrastructure
+          </option>
+          <option value="007">
+            Awareness and Preparedness Towards Disaster Management
+          </option>
+          <option value="008">
+            Investigation of vulnerabilities in implementation of crypto library
+            used by OpenVPN for Internet Protocol Security (IPsec), IPV6
+            deployment.
+          </option>
+          <option value="009">
+            Develop a AI/ML tool to detect whether a system / firewall /router /
+            network is compromised. The technique should not rely only on IoCs
+            (Indicators of Compromises) detection.
+          </option>
+          <option value="010">
+            Developing a GUI based hardening script for Ubuntu operating system
+            with flexibility to cater for organisational security policies
+          </option>
+        </select>
+      </span>
+            <!-- Presentation -->
+            <span>
+          <label for="usrPresentation">Presentation:</label>
+          <input
+                  type="file"
+                  accept=".ppt, .pptx, .pdf"
+                  id="usrPresentation"
+                  name="usrPresentation"
+                  required /><br /><br />
+        </span>
+            <input class="submit--btn" type="submit" value="SUBMIT" name="hackathon-submit" />
+        </form>
+        <?php
+
+        if(isset($user->errorMessage)){
+            echo $user->errorMessage;
+        }
+
+        if(isset($fileUploader->errorMessage)){
+            echo $fileUploader->errorMessage;
+        }
+        ?>
+    </div>
+</div>
+
+<?php }//end else here ?>
